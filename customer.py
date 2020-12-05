007e20bd98db54786b5968ac8a759f9b6637dc57
Boyan <bx376@nyu.edu>
2020-12-05 23:28:23 +0800

    fix

---
+++ customer.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+from appconf import app, conn
+from forms import *
+
+@app.route("/customerHome", methods=["GET"])
+def customerHome():
+    username = session["username"]
+
+    query = """
+                SELECT purchases.ticket_id,
+                ticket.airline_name,
+                ticket.flight_num,
+                flight.departure_airport,
+                flight.departure_time,
+                flight.arrival_airport,
+                flight.arrival_time,
+                flight.price,
+                flight.status,
+                flight.airplane_id
+                FROM purchases, ticket, flight
+                        WHERE purchases.customer_email = %s
+                        AND purchases.ticket_id = ticket.ticket_id
+                        AND ticket.airline_name = flight.airline_name
+                        AND ticket.flight_num = flight.flight_num
+                        AND departure_time > curdate();
+    """
+
+    cursor = conn.cursor()
+    cursor.execute(query, (username))
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("pages/customerHome.html", username=username, posts=data)
+
+
+@app.route("/customerHome/purchase", methods=["GET"])
+def customerPurchase():
+    username = session["username"]
+    form = PurchaseForm(request.form)
+    return render_template('pages/customerPurchase.html', username=username, form=form)
+
+
+@app.route('/customerHome/search', methods=['GET'])
+def customerSearch():
+    username = session["username"]
+    form = FlightSearchForm(request.form)
+    return render_template('pages/customerSearch.html', username=username, form=form)
+
+
+@app.route('/customerHome/search/results', methods=['GET', 'POST'])
+def customerSearchHandler():
+
+    fromCity = request.form['fromCity']
+    fromAirport = request.form['fromAirport']
+    fromDate = request.form['fromDate']
+    toCity = request.form['toCity']
+    toAirport = request.form['toAirport']
+    toDate = request.form['toDate']
+    query = """SELECT distinct f.airline_name,
+                f.flight_num,
+                departure_airport,
+                departure_time,
+                arrival_airport,
+                arrival_time,
+                price,
+                airplane_id
+                    FROM flight as f, airport
+                    WHERE airport.airport_name=f.departure_airport
+                    AND airport.airport_city = %s
+                    AND airport.airport_name = %s
+                    AND %s BETWEEN DATE_SUB(f.departure_time, INTERVAL 2 DAY) AND DATE_ADD(f.departure_time, INTERVAL 2 DAY)
+                    AND %s BETWEEN DATE_SUB(f.arrival_time, INTERVAL 2 DAY) AND DATE_ADD(f.arrival_time, INTERVAL 2 DAY)
+                    AND (f.airline_name, f.flight_num) in
+                        (SELECT flight.airline_name, flight.flight_num FROM flight, airport
+                            WHERE airport.airport_name=flight.arrival_airport
+                            AND airport.airport_city = %s
+                            AND airport.airport_name = %s)
+                    AND (SELECT DISTINCT seats FROM flight, airplane
+                            WHERE flight.airplane_id = airplane.airplane_id
+                            AND flight.airline_name = airplane.airline_name
+                            AND flight.airline_name = f.airline_name
+                            AND flight.flight_num = f.flight_num) >=
+                                (SELECT COUNT(*) FROM ticket
+                                    WHERE ticket.airline_name = f.airline_name
+                                    AND ticket.flight_num = f.flight_num)
+    """
+
+    cursor = conn.cursor()
+    cursor.execute(query, (fromCity, fromAirport, fromDate, toDate, toCity, toAirport))
+    data = cursor.fetchall()
+    cursor.close()
+
+    if(data):
+        username = session["username"]
+        form = FlightSearchForm(request.form)
+        return render_template('pages/customerSearch.html', username=username, form=form, results=data)
+    else:
+        #returns an error message to the html page
+        username = session["username"]
+        form = FlightSearchForm(request.form)
+        error = 'No results found'
+        return render_template('pages/customerSearch.html', username=username, form=form, error=error)
---
+++ docs/test_query_data.sql
@@ -0,0 +1,52 @@
+-- @app.route("/customerHome")
+SELECT purchases.ticket_id,
+       ticket.airline_name,
+       ticket.flight_num,
+       flight.departure_airport,
+       flight.departure_time,
+       flight.arrival_airport,
+       flight.arrival_time,
+       flight.price,
+       flight.status,
+       flight.airplane_id
+       FROM purchases, ticket, flight
+            WHERE purchases.customer_email = 'one@nyu.edu'
+              AND purchases.ticket_id = ticket.ticket_id
+              AND ticket.airline_name = flight.airline_name
+              AND ticket.flight_num = flight.flight_num
+              AND departure_time > curdate()
+
+
+-- @app.route("/customerSearch")
+
+-- Query Format: fromCity,      fromAirport, fromDate,   toDate,     toCity,        toAirport
+-- Query Input : San Francisco, SFO,         2020-12-20, 2020-12-21, New York City, JFK
+SELECT distinct f.airline_name,
+                f.flight_num,
+                departure_airport,
+                departure_time,
+                arrival_airport,
+                arrival_time,
+                price,
+                airplane_id
+            FROM flight as f, airport
+            WHERE airport.airport_name=f.departure_airport
+            AND airport.airport_city = 'San Francisco'
+            AND airport.airport_name = 'SFO'
+            AND '2020-12-20' BETWEEN DATE_SUB(f.departure_time, INTERVAL 2 DAY) AND DATE_ADD(f.departure_time, INTERVAL 2 DAY)
+            AND '2020-12-21' BETWEEN DATE_SUB(f.arrival_time, INTERVAL 2 DAY) AND DATE_ADD(f.arrival_time, INTERVAL 2 DAY)
+            AND (f.airline_name, f.flight_num) in
+                (SELECT flight.airline_name, flight.flight_num FROM flight, airport
+                    WHERE airport.airport_name=flight.arrival_airport
+                      AND airport.airport_city = 'New York City'
+                      AND airport.airport_name = 'JFK')
+            AND (SELECT DISTINCT seats FROM flight, airplane
+                    WHERE flight.airplane_id = airplane.airplane_id
+                      AND flight.airline_name = airplane.airline_name
+                      AND flight.airline_name = f.airline_name
+                      AND flight.flight_num = f.flight_num) >=
+                        (SELECT COUNT(*) FROM ticket
+                            WHERE ticket.airline_name = f.airline_name
+                              AND ticket.flight_num = f.flight_num)
+-- Response Format: airline_name, flight_num,  departure_airport, departure_time, arrival_airport, airplane_id, arrival_time, price
+-- Response Record: Jet Blue,     139,         SFO,               2020-12-20,     JFK,             2020-12-21
